7b5304fd32a86571f04c493d951201e92c152251
Sammie Collinson <heroldsamantha@gmail.com>
2022-02-03 10:37:09 -0500

    created pageSetter variable on Form to conditionally render components based on currentPage state. Pulled props through to LandingPage.jsx.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,19 +6,28 @@ import Thanks from './Thanks';
 
 const Form = (props) => {
     const { currentPage, incrementPage, decrementPage, name, age, email, handleChange } = props
+
+    let pageSetter;
+    if(currentPage===0){
+        pageSetter = <LandingPage
+            incrementPage={incrementPage}
+            currentPage={currentPage}  />
+    } if(currentPage===1){
+        pageSetter = <Info
+            currentPage={currentPage}
+            incrementPage={incrementPage}
+            decrementPage={decrementPage}
+            name={name}
+            age={age}
+            email={email}
+            handleChange={handleChange} />
+    } if(currentPage===2){
+        pageSetter = <Thanks />
+    }
+
     return(
         <div>
-            <LandingPage />
-            <Info
-                currentPage={currentPage}
-                incrementPage={incrementPage}
-                decrementPage={decrementPage}
-                name={name}
-                age={age}
-                email={email}
-                handleChange={handleChange} />
-            <ErrorPage />
-            <Thanks />
+            {pageSetter}
         </div>
     )
 }
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
-const LandingPage = () => {
+const LandingPage = (props) => {
+    const { incrementPage } = props
     return (
         <div>
             <h1>Welcome to my Website!</h1>
             <h3>Please click below to sign up.</h3>
-            <button>Sign Up Here</button>
+            <button onClick={incrementPage}>Sign Up Here</button>
         </div>
     )
 
